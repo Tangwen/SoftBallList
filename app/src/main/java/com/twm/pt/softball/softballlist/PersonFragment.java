e6c848430199fa8756760c80cbba86c7306b8d75
TangWen <tangwen@taiwanmobile.com>
2015-04-22 18:09:58 +0800

    initial

--- app/src/main/java/com/twm/pt/softball/softballlist/PersonFragment.java
+++ app/src/main/java/com/twm/pt/softball/softballlist/PersonFragment.java
@@ -3,6 +3,7 @@ package com.twm.pt.softball.softballlist;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
